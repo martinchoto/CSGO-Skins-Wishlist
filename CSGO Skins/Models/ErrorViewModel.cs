70d75ac092cec4f67c6a03bfb9f7b1c6d3cb331e
martinchoto <martin_stalev03@abv.bg>
2024-12-25 13:51:40 +0200

    git init

---
+++ CSGO Skins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSGO_Skins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
